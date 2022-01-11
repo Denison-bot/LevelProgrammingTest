aed9145348ffc0815c95adfd277211865f95b7cf
Denison Percey <denisonipercey@outlook.com>
2022-01-11 08:51:48 -0400

    Initial commit

---
+++ Assets/scripts/Rotate.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var speed = 50;
+        transform.Rotate(0, speed*Time.deltaTime, 0);
+    }
+}
